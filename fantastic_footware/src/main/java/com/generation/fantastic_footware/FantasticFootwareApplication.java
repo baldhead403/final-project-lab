e99f436cd59928409c08ff225f6c4bb5414ce184
Donald Rivers <donaldrivers76@gmail.com>
2022-01-18 12:15:19 -0500

    Add files via upload

---
+++ fantastic_footware/src/main/java/com/generation/fantastic_footware/FantasticFootwareApplication.java
@@ -0,0 +1,13 @@
+package com.generation.fantastic_footware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasticFootwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasticFootwareApplication.class, args);
+	}
+
+}
---
+++ fantastic_footware/src/test/java/com/generation/fantastic_footware/FantasticFootwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.fantastic_footware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasticFootwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
